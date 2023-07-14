057ba15ed2cf2c6b165b59efd04fa78e34fd6724
Berik <berloo24@gmail.com>
2023-07-14 15:58:22 +0600

    AddHeader

--- components/main.tsx
+++ components/main.tsx
@@ -7,9 +7,9 @@ const Main = () => {
     <div className="main">
         <div className="container">
             <div className="main_wrapper">
-                <h1 className="main_wrapper_title">
+                <div className="main_wrapper_title">
                 Make your dream house real.
-                </h1>
+                </div>
             </div>
         </div>
     </div>
